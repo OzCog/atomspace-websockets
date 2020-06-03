e29b3155b02a11ecd56688cf810879879df17b01
Abdulrahman Semrie <hsamireh@gmail.com>
2020-06-03 14:34:30 +0300

    Make linking against bioscience optional

---
+++ sample.scm
@@ -0,0 +1,23 @@
+(EvaluationLink
+  (Predicate "is_part")
+  (Concept "A")
+  (Concept "B")
+)
+
+(EvaluationLink
+  (Predicate "is_part")
+  (Concept "A")
+  (Concept "C")
+)
+
+(EvaluationLink
+  (Predicate "is_part")
+  (Concept "C")
+  (Concept "D")
+)
+
+(EvaluationLink
+  (Predicate "is_part")
+  (Concept "A")
+  (Concept "E")
+)
